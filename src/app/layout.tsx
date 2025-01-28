abb29e886e2d6aae15182b3e5c77436ea11dab57
DeonSmithCoZa <hello@deonsmith.co.za>
2025-01-28 09:12:43 +0200

    add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Gajraj_One, Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -39,12 +40,18 @@ export default function RootLayout({
         <Header />
         {children}
       </body>
+      <GoogleAnalytics gaId="G-Z1R7FGJEQG" />
     </html>
   );
 }
 
 const Header: React.FC = () => (
   <header className="p-4 mb-8">
-    <h1 className="text-center text-8xl font-gajraj"><span className="inline-block bg-[#DFA] text-black rounded-md px-3 mr-1">b</span>EDH</h1>
+    <h1 className="text-center text-8xl font-gajraj">
+      <span className="inline-block bg-[#DFA] text-black rounded-md px-3 mr-1">
+        b
+      </span>
+      EDH
+    </h1>
   </header>
 );
