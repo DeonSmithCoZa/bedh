86fd3b069f27354f5428b100a3f027dde7825c1c
DeonSmithCoZa <hello@deonsmith.co.za>
2025-01-30 12:18:37 +0200

    tweak rules text

--- src/app/components/rules.tsx
+++ src/app/components/rules.tsx
@@ -8,16 +8,19 @@ const Rules: React.FC = () => (
     <ul className="list-disc list-inside">
       <li>
         $50 budget (using TCG Player pricing) for the entire deck, excluding
-        basic lands.
+        basic lands and your commander.
       </li>
       <li>
         Random commander colour identity assigned. Only 1, 2 and 3 colour.
       </li>
       <li>
-        No commander from the top 100 on EDHRec. No restriction on the 99.
+        No commander from the top 100 on EDHRec. No restriction on the 99. If
+        running partners, backgrounds, etc. both need to be outside of the top
+        100.
       </li>
       <li>
-        No individual card over $5 in value (looking at the cheapest printing).
+        No individual card over $5 in value (looking at the cheapest printing)
+        including the commander.
       </li>
       <li>All normal commander rules apply (including the banlist)</li>
       <li>
