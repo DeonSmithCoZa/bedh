857e4b8da90f7a548413211dfdce0bfdbfaafd14
DeonSmithCoZa <hello@deonsmith.co.za>
2025-02-14 17:04:19 +0200

    try something else

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+  },
 };
 
 export default nextConfig;
